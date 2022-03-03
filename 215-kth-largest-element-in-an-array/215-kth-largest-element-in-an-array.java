c2192ede1cecbc97a34533c42d66bd2cc333a3a8
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-03-04 03:37:23 +0530

    Time: 4 ms (79.38%), Space: 44.2 MB (40.66%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -0,0 +1,110 @@
+class Solution {
+    
+    class MaxHeap {
+        int[] heap;
+        int size;
+
+        public MaxHeap(int size) {
+            this.heap = new int[size];
+            this.size = 0;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public int[] getHeap() {
+            return this.heap;
+        }
+
+        public boolean isEmpty() {
+            if (this.size == 0) {
+                return true;
+            }
+            return false;
+        }
+
+        public boolean isFull() {
+            if (this.size == heap.length) {
+                return true;
+            }
+            return false;
+        }
+
+        public void insert(int x) {
+
+            // insert x into last position
+            if (!isFull()) {
+                heap[size] = x;
+                size++;
+
+                int i = size - 1;
+                while (i > 0) {
+                    int parentIndex = (i - 1) / 2;
+                    if (heap[i] > heap[parentIndex]) {
+                        swap(i, parentIndex);
+                        i = parentIndex;
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+
+        public int getMaximum() {
+            int x = -1;
+            if (!isEmpty()) {
+                x = heap[0];
+                // swap last element and x
+                swap(0, size - 1);
+                heap[size - 1] = -1;
+                size--;
+
+                int i = 0;
+
+                while ((2 * i) + 1 < size) {
+                    int leftChildIndex = (2 * i) + 1;
+                    int rightChildIndex = (2 * i) + 2;
+                    int max = Math.max(heap[leftChildIndex], heap[i]);
+                    if (rightChildIndex < size) {
+                        max = Math.max(max, heap[rightChildIndex]);
+                    }
+
+                    if (max == heap[i]) {
+                        break;
+                    } else if (max == heap[leftChildIndex]) {
+                        swap(i, leftChildIndex);
+                        i = leftChildIndex;
+                    } else if (rightChildIndex < size && max == heap[rightChildIndex]) {
+                        swap(i, rightChildIndex);
+                        i = rightChildIndex;
+                    }
+                }
+            }
+            return x;
+        }
+
+        private void swap(int i, int j) {
+            int temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+        }
+
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        
+        MaxHeap maxHeap = new Solution().new MaxHeap(nums.length);
+        for(int i=0;i<nums.length;i++) {
+            maxHeap.insert(nums[i]);
+        }
+        
+        int answer = 0;
+        while(k>0) {
+            answer = maxHeap.getMaximum();
+            k--;
+        }
+        return answer;
+        
+    }
+}
