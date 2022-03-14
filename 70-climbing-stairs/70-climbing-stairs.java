8109f9e0efdd0298b222c2a798a819e36ba1323a
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-03-14 20:06:29 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (46.88%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -5,18 +5,25 @@ class Solution {
             return n;
         }
         
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
         
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 2;
+        int p3 = 0;
+        int p2 = 1;
+        int p1 = 2;
         
         for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            // dp[i] = dp[i-1] + dp[i-2];
+            p3 = p2;
+            p2 = p1;
+            p1 = p2 + p3;
         }
         
-        return dp[n];
+        // return dp[n];
+        return p1;
         
     }
 }
