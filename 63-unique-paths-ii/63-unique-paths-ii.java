6cd562a9ffc6ca613dd91c93aa734ee455c6fa7d
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-03-18 21:54:04 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (76.31%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,5 +1,52 @@
 class Solution {
+    
+    /**
+    * Approach : 1d dp array of size m
+    SC: O(m)
+    **/
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int dp[] = new int[m];
+        
+        // fill first row
+        for(int j=0;j<m; j++) {
+            if(obstacleGrid[0][j] == 1) {
+                // obstacle
+                dp[j] = 0;
+            } else {
+                if(j ==0) {
+                    dp[j] = 1;
+                } else {
+                    dp[j] = dp[j-1];
+                }
+            }
+        }
+        
+        // fill remaining elements
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    // obstacle
+                    dp[j] = 0;
+                } else if(j==0) {
+                    dp[j] = dp[j];
+                } else {
+                    dp[j] = dp[j] + dp[j-1];
+                }
+            }
+        }
+        
+        return dp[m-1];        
+    }
+    
+    
+    /**
+    * Approach: with 2d dp array
+    SC: O(n x m)
+    **/
+    public int uniquePathsWithObstacles_2ddparray(int[][] obstacleGrid) {
         
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length;
