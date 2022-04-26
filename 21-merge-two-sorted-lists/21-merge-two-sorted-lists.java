06db6a7d1dfdb9ca55906471f1c4d18ff102c3fc
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-04-26 17:44:10 +0530

    Time: 1 ms (68.02%), Space: 42.8 MB (65.49%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,46 +10,44 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head;
         
-        if(list1 == null) {
-            return list2;
-        }
-        if(list2 == null) {
-            return list1;
+        if(list1 == null && list2 == null ) return null;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        
+        if(list1.val <= list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
         }
         
         ListNode t1 = list1;
         ListNode t2 = list2;
-        ListNode result = null;
-        
-        if(t1.val < t2.val) {
-            result = t1;
-            t1 = t1.next;
-        } else {
-            result = t2;
-            t2= t2.next;
-        }
-        
-        ListNode temp = result;
-        while(t1!=null && t2!=null) {
-            if(t1.val < t2.val) {
-                temp.next = t1;
-                temp = temp.next;
+        ListNode curr = head;
+        while(t1 != null && t2 != null) {
+            
+            if(t1.val <= t2.val) {
+                curr.next = t1;
                 t1 = t1.next;
             } else {
-                temp.next = t2;
-                temp = temp.next;
-                t2= t2.next;
+                curr.next = t2;
+                t2 = t2.next;
             }
+            curr = curr.next;
         }
         
-        if(t1!=null) {
-            temp.next = t1;
+        if(t1 != null) {
+            curr.next = t1;
         }
-        if(t2!=null) {
-            temp.next = t2;
+        if(t2 != null) {
+            curr.next = t2;
         }
-        return result;
+        
+        return head;
+        
         
     }
 }
