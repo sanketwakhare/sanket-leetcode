f908c3072d7ec5574f0cf39b4b38d79756ac62f7
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-05-03 22:41:41 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (53.02%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,20 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
         
+        // initialize memoizaed/dp array
         int[] memo = new int[nums.length];
         Arrays.fill(memo, -1);
+        
+        // function call
         return maxSum(nums.length-1, nums, memo);
     }
     
     public int maxSum(int n, int[] nums, int[] memo) {
         
+        // base case
         if(n < 0) return 0;
         
+        // reuse if repeated subproblems occur
         if(memo[n]!= -1) return memo[n];
         
+        // consider both possibilities
         int x = nums[n] + maxSum(n-2, nums, memo);
         int y = maxSum(n-1, nums, memo);
         
+        // return max sum out of 2 possibilities
         return memo[n] = Math.max(x, y);
     }
 }
