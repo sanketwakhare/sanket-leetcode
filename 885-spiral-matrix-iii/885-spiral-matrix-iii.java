3b366ac760bb41009f37f90f74699d5effe794ca
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-05-17 20:37:20 +0530

    Time: 4 ms (80.72%), Space: 42.8 MB (99.04%) - LeetHub

--- 885-spiral-matrix-iii/885-spiral-matrix-iii.java
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.java
@@ -1,16 +1,22 @@
-class Solution {
+    class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        // total no of cells
         int cells = rows * cols;
         int mat[][] = new int[cells][2];
 
+        // start with a cell in next row
         int i = rStart + 1;
         int j = cStart;
         int n = 2;
         int m = 2;
 
+        // spiral traversal from inner to outer boundary
         int cellCount = 0;
         while (cellCount < cells) {
 
+            // Boundary Printing
+
+            // print starting column in reverse direction except starting element
             for (int k = 0; k < n - 1; k++) {
                 i--;
                 if (isSafe(i, j, rows, cols)) {
@@ -18,6 +24,7 @@ class Solution {
                     cellCount++;
                 }
             }
+            // print first row except starting element
             for (int k = 0; k < m - 1; k++) {
                 j++;
                 if (isSafe(i, j, rows, cols)) {
@@ -25,6 +32,7 @@ class Solution {
                     cellCount++;
                 }
             }
+            // print last column except starting element
             for (int k = 0; k < n - 1; k++) {
                 i++;
                 if (isSafe(i, j, rows, cols)) {
@@ -32,6 +40,7 @@ class Solution {
                     cellCount++;
                 }
             }
+            // print last row except starting element
             for (int k = 0; k < m - 1; k++) {
                 j--;
                 if (isSafe(i, j, rows, cols)) {
@@ -40,8 +49,10 @@ class Solution {
                 }
             }
 
+            // go to next level outside current boundary
             m = m + 2;
             n = n + 2;
+            // start position for outer boundary printing
             i++;
             j--;
         }
@@ -49,6 +60,7 @@ class Solution {
         return mat;
     }
 
+    // is valid cell
     public boolean isSafe(int i, int j, int rows, int cols) {
         boolean isSafe = false;
         isSafe = (i >= 0 && i < rows && j >= 0 && j < cols);
