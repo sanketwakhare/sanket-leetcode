a6bde53e645fdf29d1557db2be977cc2a54f1be0
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-05-17 12:52:52 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (78.57%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        List<Integer> result = new ArrayList<Integer>();
+        if(n == 1) {
+            for(int ele: matrix[0]) {
+                result.add(ele);
+            }
+            return result;
+        }
+        if(m == 1) {
+            for(int[] row: matrix) {
+                result.add(row[0]);
+            }
+            return result;
+        }
+        
+        int i = 0;
+        int j = 0;        
+        while(n > 1 && m > 1) {
+            
+            for(int k=0; k<m-1; k++) {
+                result.add(matrix[i][j++]);
+            }
+            for(int k=0; k<n-1; k++) {
+                result.add(matrix[i++][j]);
+            }
+            for(int k=0; k<m-1; k++) {
+                result.add(matrix[i][j--]);
+            }
+            for(int k=0; k<n-1; k++) {
+                result.add(matrix[i--][j]);
+            }
+            
+            n = n-2;
+            m = m-2;
+            i++;
+            j++;                        
+        }
+        
+        if(n == 0 || m == 0) {
+            return result;
+        } else if(n >= m) {
+            // print column
+            for(int k=0; k<n; k++) {
+                result.add(matrix[i++][j]);
+            }
+        } else if(m > n) {
+            // print row
+            for(int k=0; k<m; k++) {
+                result.add(matrix[i][j++]);
+            }
+        }
+        
+        return result;        
+    }
+}
