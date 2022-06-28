7f106f82f1957bba027bdd1901110d55649c7e84
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-06-28 09:50:12 +0530

    Time: 214 ms (52.80%), Space: 46.6 MB (86.93%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
@@ -4,6 +4,7 @@
  */
 var minDeletions = function(s) {
     
+    // build map
     let map = new Map();
     for(let i=0; i< s.length; i++) {
         let currChar = s.charAt(i);
@@ -14,22 +15,29 @@ var minDeletions = function(s) {
         }
     }
     
+    // build frequency array
     let freqArray = [];
     for(let value of map.values()) {
         freqArray.push(value);
     }
+    // sort in descending order
     freqArray.sort((a,b) => b-a);
-    console.log(freqArray);
+    
     
     let maxAllowed = freqArray[0]-1;
     let count = 0;
     for(let i=1; i< freqArray.length; i++) {
         if(freqArray[i] > maxAllowed) {
+            // update count as we have to delete those many characters
             count += freqArray[i] - maxAllowed;
-            maxAllowed = Math.max(0, maxAllowed - 1);
+            // update maxAllowed
+            maxAllowed = maxAllowed - 1;
         } else {
-            maxAllowed = Math.max(0, freqArray[i] - 1);
+            // update maxAllowed to current freq - 1 as arrayis sorted in descending order
+            maxAllowed = freqArray[i] - 1;
         }
+        // make sure maxAllowed does not go less than 0
+        maxAllowed = Math.max(0, maxAllowed);
     }
     return count;
     
