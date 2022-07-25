a7d890cc610a45c3a47623fc075bbdd38089e367
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-07-25 19:14:21 +0530

    Time: 80 ms (41.31%), Space: 110.7 MB (84.61%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
         
+        // store all candle indices
         int n = s.length();
         List<Integer> candleIndices = new ArrayList<>();
         for(int i=0; i<n; i++) {
             if(s.charAt(i) == '|') candleIndices.add(i);
         }
         
+        // for every query
+        // 1. find ceil of the left index -> first candle within range [l, r]
+        // 2. find floor of the right index -> last candle within range [l, r]
+        // 3. find no of plates wihtin range [l, r]
+        // 4. subtract no of candles within the range [l, r]
         int[] output = new int[queries.length];
         for(int i=0; i< queries.length; i++) {
             int l = queries[i][0];
@@ -30,6 +36,7 @@ class Solution {
         return output;
     }
     
+    // binary search find floor
     public int findFloor(List<Integer> list, int target) {
         int start = 0;
         int end = list.size() - 1;
@@ -49,6 +56,7 @@ class Solution {
         return answer;
     }
     
+    // binary search find ceil
     public int findCeil(List<Integer> list, int target) {
         int start = 0;
         int end = list.size() - 1;
