972c660256270f649672d48edceef154949dfbea
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-08-04 08:33:10 +0530

    Time: 0 ms (100.00%), Space: 41 MB (45.83%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        int ext = 1 * p;
+        int ref = 1 * q;
+        while (ext % 2 == 0 && ref % 2 == 0) {
+            ext /= 2;
+            ref /= 2;
+        }
+        if (ext % 2 == 0) {
+            return 2;
+        } else if (ref % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
