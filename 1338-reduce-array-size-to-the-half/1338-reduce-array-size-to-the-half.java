904c4139c461d1a210b17aa9247b463016f3a76e
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-08-18 22:44:21 +0530

    Time: 153 ms (15.85%), Space: 93.6 MB (30.36%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -5,16 +5,33 @@ class Solution {
             int currFreq = freqMap.getOrDefault(ele, 0);
             freqMap.put(ele, currFreq + 1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int key: freqMap.keySet()) {
-            pq.add(freqMap.get(key));
+        Map<Integer, List<Integer>> map = new TreeMap<>(Collections.reverseOrder());
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            List<Integer> list = map.getOrDefault(value, new ArrayList<Integer>());
+            list.add(key);
+            map.put(value, list);
         }
+
         int n = arr.length;
-        int count = 0;
         int expectedSize = n / 2;
-        while(expectedSize > 0) {
-            expectedSize -= pq.poll();
-            count++;
+        int count = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            int freq = entry.getKey();
+            List<Integer> list = entry.getValue();
+            if (freq * list.size() <= expectedSize) {
+                expectedSize -= freq * list.size();
+                count += list.size();
+            } else {
+                int size = list.size();
+                while (size >= 0 && expectedSize > 0) {
+                    expectedSize -= freq;
+                    count += 1;
+                    size--;
+                }
+            }
+            if (expectedSize <= 0) break;
         }
         return count;
     }
