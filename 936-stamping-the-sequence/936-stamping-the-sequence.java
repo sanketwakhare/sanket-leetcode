b870bb4aa8d2026007408eac3d9589164184b53f
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-08-21 20:11:05 +0530

    Time: 33 ms (24.11%), Space: 54.3 MB (16.96%) - LeetHub

---
+++ 936-stamping-the-sequence/936-stamping-the-sequence.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] movesToStamp(String stamp, String target) {
+
+        int tlength = target.length();
+        int slength = stamp.length();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("?".repeat(tlength));
+        String targetOutput = sb.toString();
+
+        sb = new StringBuilder();
+        sb.append("?".repeat(slength));
+        String alreadyStamped = sb.toString();
+
+        List<Integer> list = new LinkedList<>();
+        boolean canPerform = true;
+        while (!target.equals(targetOutput) && canPerform) {
+            canPerform = false;
+            int ind = target.indexOf(stamp);
+            if (ind != -1) {
+                String temp = target.substring(ind, ind + slength);
+                target = replace(temp, target, ind);
+                list.add(0, ind);
+                canPerform = true;
+            } else {
+                for (int i = 0; i <= tlength - slength; i++) {
+                    String temp = target.substring(i, i + slength);
+                    if (!alreadyStamped.equals(temp) && canReplace(temp, stamp)) {
+                        // replace
+                        target = replace(temp, target, i);
+                        list.add(0, i);
+                        canPerform = true;
+                    }
+                }
+            }
+        }
+        if (!canPerform) return new int[]{};
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    public boolean canReplace(String temp, String stamp) {
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp.charAt(i) != stamp.charAt(i) && temp.charAt(i) != '?') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String replace(String temp, String target, int index) {
+        StringBuilder sb = new StringBuilder(target);
+        for (int i = index; i < index + temp.length(); i++) {
+            sb.setCharAt(i, '?');
+        }
+        return sb.toString();
+    }
+}
