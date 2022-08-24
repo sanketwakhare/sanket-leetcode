454312a1ef5ddecdd23ee8b85917293c1c2c8892
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-08-24 23:34:17 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (60.39%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
@@ -14,23 +14,30 @@
  * }
  */
 class Solution {
-    int answer = 0;
-    int maxLevel = 0;
     public int findBottomLeftValue(TreeNode root) {
-        traverse(root, 1);
-        return answer;
+        // result[0] = maxLevel
+        // result[1] = answer
+        int[] result = new int[] {0, 0};
+        
+        traverse(root, 1, result);
+        return result[1];
     }
     
-    public void traverse(TreeNode root, int level) {
+    public void traverse(TreeNode root, int level, int[] result) {
+        
+        // base condition
         
         if(root == null) return;
         
-        if(level > maxLevel) {
-            maxLevel = level;
-            answer = root.val;
+        if(level > result[0]) {
+            // if current level is max level till this point, update answer and max level
+            result[0] = level;
+            result[1] = root.val;
         }
         
-        traverse(root.left, level + 1);
-        traverse(root.right, level + 1);
+        // traverse left subtree
+        traverse(root.left, level + 1, result);
+        // traverse right subtree
+        traverse(root.right, level + 1, result);
     }
 }
