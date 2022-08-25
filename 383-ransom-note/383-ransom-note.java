c35823c23a4fa670a7eb2886431bddeb6c68afc4
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-08-25 08:47:02 +0530

    Time: 12 ms (54.41%), Space: 42.7 MB (92.83%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        // build frequency map with magazine word
         Map<Character, Integer> map = new HashMap<>();
         for(int i=0; i< magazine.length(); i++)  {
             char ch = magazine.charAt(i);
             int freq = map.getOrDefault(ch, 0);
             map.put(ch, freq + 1);
         }
+        // iterate over ransomeNote word and check if it can be formed by characters in magazine word
         for(int i=0; i< ransomNote.length(); i++) {
             char ch = ransomNote.charAt(i);
             int freq = map.getOrDefault(ch, 0);
+            // if current character can not be formed, return false
             if(freq == 0) {
                 return false;
             }
             map.put(ch, freq - 1);
         }
+        // ransomWord can be formed by using characters in magazine
         return true;
     }
 }
