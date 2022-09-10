28c24be40ec3b6afd3728f296296265fc01ecbf6
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2022-09-10 22:49:18 +0530

    Time: 402 ms (5.09%), Space: 140.6 MB (10.95%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 2 transactions cap is allowed
+        int cap = 2;
+        int n = prices.length;
+        int[][][] dp = new int[n + 1][2][cap + 1];
+        for (int index = 0; index < dp.length; index++) {
+            for (int buy = 0; buy < dp[index].length; buy++) {
+                Arrays.fill(dp[index][buy], -1);
+            }
+        }
+
+        return f(0, true, cap, n, prices, dp);
+    }
+    
+    public int f(int index, boolean canBuy, int cap, int n, int[] prices, int[][][] dp) {
+        if (index == n || cap == 0) return 0;
+        int profit;
+        if (canBuy) {
+            if (dp[index][0][cap] != -1) return dp[index][0][cap];
+            int buy = -prices[index] + f(index + 1, false, cap, n, prices, dp);
+            int dontBuy = f(index + 1, true, cap, n, prices, dp);
+            dp[index][0][cap] = profit = Math.max(buy, dontBuy);
+        } else {
+            if (dp[index][1][cap] != -1) return dp[index][1][cap];
+            int sell = prices[index] + f(index + 1, true, cap - 1, n, prices, dp);
+            int dontSell = f(index + 1, false, cap, n, prices, dp);
+            dp[index][1][cap] = profit = Math.max(sell, dontSell);
+        }
+        return profit;
+    }
+}
