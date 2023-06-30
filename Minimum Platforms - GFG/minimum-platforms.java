46b5b68ad43e9fac944ddcaf3c0e0bdefab14160
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2023-06-30 11:32:22 +0530

    Added solution - LeetHub

---
+++ Minimum Platforms - GFG/minimum-platforms.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int max = 0;
+        int curr = 0;
+        int i = 0;
+        int j = 0;
+        
+        while(i<n && j<n) {
+            if(arr[i] <= dep[j]) {
+                curr++;
+                i++;
+            } else {
+                curr--;
+                j++;
+            }
+            max = Math.max(max, curr);
+        }
+        
+        return max;
+    }
+    
+}
+
